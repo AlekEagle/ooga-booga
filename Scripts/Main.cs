f5c393eb6e0f7b762486f8b2736e543be719e39f
treeman0013 <foregoneocean89@gmail.com>
2021-01-04 17:06:16 -0700

    Changes to seed

--- Scripts/Main.cs
+++ Scripts/Main.cs
@@ -3,37 +3,46 @@ using System;
 
 public class Main : Control
 {
-  public TextureRect thing;
+  public TextureRect textRect;
 
-  public Image otherThing;
+  public Image image;
   public RandomNumberGenerator rng = new RandomNumberGenerator();
+
+  public uint seed = 0;
 
   public override void _Ready()
   {
-    thing = GetNode<TextureRect>("TextureRect");
-    otherThing = new Image();
-    otherThing.Create(128, 128, false, Image.Format.Rgb8);
-    otherThing.Lock();
+	textRect = GetNode<TextureRect>("TextureRect");
+	image = new Image();
+	image.Create(128, 128, false, Image.Format.Rgb8);
+	image.Lock();
 
-    for (int x = 0; x < 128; x++)
-    {
-      for (int y = 0; y < 128; y++)
-      {
-        rng.Randomize();
-        float r = rng.Randf(), g = rng.Randf(), b = rng.Randf();
-        otherThing.SetPixel(x, y, new Color(r, g, b));
-      }
-    }
+	for (int x = 0; x < 128; x++)
+	{
+	  for (int y = 0; y < 128; y++)
+	  {
+		rng.Randomize();
+		float r = rng.Randf(), g = rng.Randf(), b = rng.Randf();
+		image.SetPixel(x, y, new Color(r, g, b));
+		seed += (uint)new Color(r, g, b).ToArgb32();
+	  }
+	}
 
-    ImageTexture a = new ImageTexture();
-    a.CreateFromImage(otherThing);
+	ImageTexture a = new ImageTexture();
+	a.CreateFromImage(image);
 
-    thing.Texture = a;
-    a.Flags = 0;
+	textRect.Texture = a;
+	a.Flags = 0;
+	GD.Print(seed);
   }
 
-  private void ChangeLabel(float a)
-  {
-    GetNode<Label>("Label3").Text = a.ToString();
-  }
+	private void ChangeLabel(float a)
+	{
+		GetNode<Label>("Label3").Text = a.ToString();
+	}
+
+  	private void OnExportButtonPressed()
+	{
+		image.SavePng("res:///Images/Seed" + OS.GetUnixTime() + ".png");
+	}
 }
